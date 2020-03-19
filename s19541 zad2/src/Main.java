f11aa796ae7123af78188072699e88e766db5cf2
Marcin Wałachowski <s19541@pjwstk.edu.pl>
2020-03-19 01:14:52 +0100

    Algorithm is working

--- s19541 zad2/src/Main.java
+++ s19541 zad2/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        double a=1;
+        double a=0.1;
         String trainSet="train_set.csv";
         String testSet="test_set.csv";
         Perceptron perceptron=new Perceptron(a,trainSet);
--- s19541 zad2/src/Perceptron.java
+++ s19541 zad2/src/Perceptron.java
@@ -2,21 +2,19 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Perceptron {
     List<Iris> train_set;
-    List<Double> wS;
-    double tS;
-    List<Double>wV;
-    double tV;
+    List<Double> w;
+    double t;
     double a;
     public Perceptron(double a,String trainSetFname){
         this.a=a;
         this.train_set=loadSetFromFile(trainSetFname);
-        wS=study("Iris-setosa");
-        wV=study("Iris-versicolor");
+        w=study("Iris-setosa");
     }
     private List<Iris> loadSetFromFile(String setFname){
         List<Iris> set=new ArrayList<>();
@@ -44,68 +42,76 @@ public class Perceptron {
     }
     public void checkTestFile(String testFileName){
         List<Iris>testSet=loadSetFromFile(testFileName);
+        int tmp=0;
+        int accuracyCount1=0;
+        int accuracyCount2=0;
+        int setosaCount=0;
         for(Iris iris:testSet){
-            System.out.println(iris+" "+checkIris(iris));
+            tmp++;
+            System.out.println(tmp+"."+checkIris(iris));
+            if(iris.name.equals("Iris-setosa"))
+                setosaCount++;
+            if(checkIris(iris).equals(iris.name)&&iris.name.equals("Iris-setosa"))
+                accuracyCount1++;
+            else if(checkIris(iris).equals(iris.name))
+                accuracyCount2++;
         }
+        System.out.println("Accuracy for Iris-Setosa:"+accuracyCount1+"/"+setosaCount);
+        System.out.println("Accuracy for Iris-Versicolor:"+accuracyCount2+"/"+(testSet.size()-setosaCount));
+        System.out.println("Overall accuracy:"+(accuracyCount1+accuracyCount2)+"/"+testSet.size());
     }
     public List<Double> study(String name){
         List<Double>w=new ArrayList<>();
+        for (int i = 0; i < train_set.get(0).attributes.size() ; i++) {
+            w.add(Math.random()*1);
+        }
         double t=0;
-        Iris tmp=null;
+        int y=0;
+        int d=0;
+        Collections.shuffle( train_set ) ;
         for(Iris iris:train_set){
-            if(iris.name.equals(name)&&tmp==null){
-                w=iris.attributes;
-                tmp=iris;
-            }else if(iris.name.equals(name)){
-                for (int i = 0; i < iris.attributes.size(); i++) {
-                    t+=iris.attributes.get(i)*tmp.attributes.get(i);
-                }
+            if(iris.name.equals(name)&&calcY(w,iris.attributes)>=t){
+                y=1;
+                d=1;
             }
+            else if(iris.name.equals(name)&&calcY(w,iris.attributes)<t){
+                y=0;
+                d=1;
+            }
+            else if(!iris.name.equals(name)&&calcY(w,iris.attributes)>=t){
+                y=1;
+                d=0;
+            }
+            else if(!iris.name.equals(name)&&calcY(w,iris.attributes)<t){
+                y=0;
+                d=0;
+            }
+            w=calc(w,iris.attributes,y,d);
+            t+=(d-y)*a*-1;
         }
-        for(Iris iris:train_set){
-            if(iris.name==name){
-                w=calc(w,iris.attributes,1);
-            }
-            else {
-                w = calc(w, iris.attributes, 0);
-                t=t+a*-1;
-            }
-        }
-        if(name=="Iris-setosa")
-            tS=t;
-        else
-            tV=t;
+        this.t=t;
         return w;
     }
-    public List<Double>calc(List<Double>w,List<Double>x,int y){
+    public List<Double>calc(List<Double>w,List<Double>x,int y,int d){
         List<Double>retw=new ArrayList<>();
         for (int i = 0; i < x.size(); i++) {
-            retw.add(w.get(i)+(1-y)*a*x.get(i));
+            retw.add(w.get(i)+(d-y)*a*x.get(i));
         }
         return retw;
     }
-    public boolean checkIris(Iris iris){
-        double t;
-        List<Double>w;
-        if(iris.name=="Iris-setosa") {
-            t = tS;
-            w = wS;
-        }
-        else {
-            t = tV;
-            w = wV;
-        }
+    public double calcY(List<Double>w,List<Double>x){
         double tmp=0;
-        if(w.size()!=iris.attributes.size())
-            System.out.println("podano zla ilosc argumentow");
-        else
-        for (int i = 0; i < iris.attributes.size(); i++) {
-            tmp+=iris.attributes.get(i)*wV.get(i);
+        for (int i = 0; i < x.size(); i++) {
+            tmp+=x.get(i)*w.get(i);
         }
-        System.out.println(tmp+"   "+t);
+        return tmp;
+    }
+    public String checkIris(Iris iris){
+        double tmp=calcY(w,iris.attributes);
         if(tmp>t)
-            return true;
-        return false;
+            return "Iris-setosa";
+        else
+            return "Iris-versicolor";
     }
 
 }
